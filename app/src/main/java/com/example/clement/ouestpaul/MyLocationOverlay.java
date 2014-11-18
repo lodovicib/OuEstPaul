9bc4a42a74fb77c2ca33f914e7386d6488674e55
lodovicib <lodovici.b@gmail.com>
2014-11-18 18:28:01 +0100

    Intégration de la géolocalisation et de la rehcherche

--- app/src/main/java/com/example/clement/ouestpaul/MyLocationOverlay.java
+++ app/src/main/java/com/example/clement/ouestpaul/MyLocationOverlay.java
@@ -1,13 +1,13 @@
 package com.example.clement.ouestpaul;
 
-import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationProvider;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
-import com.google.android.gms.maps.MapView;
+import com.example.clement.ouestpaul.activity.MapsActivity;
 
 /**
  * Created by Adrien on 02/11/2014.
@@ -16,13 +16,18 @@ public class MyLocationOverlay {
 
     LocationListener loc;
 Location location;
-    public static final String TAG = "YOUR-TAG-NAME";
+    MapsActivity mMapActivity;
+    public static final String TAG = "LocationOverlay";
 /*    public MyLocationOverlay(Context context, MapView carte) {
 
     }*/
 
-    public MyLocationOverlay(Location _location) {
+    public MyLocationOverlay(Location _location, MapsActivity g) {
+        mMapActivity = g;
         location = _location;
+ //       location.setAltitude(0);
+  //      location.setLongitude(0);
+  //      Log.d("test", "Location reçue dans la boucle: blablala ");
         loc = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
@@ -48,18 +53,29 @@ Location location;
                     default:
                         break;
                 }
+                Toast.makeText(mMapActivity, buf, Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onProviderEnabled(String s) {
+
                 Log.d(TAG, "Dispositif activé : "+s);
+                String msg = String.format("The provider"+ s +"is now enabled");
+                Toast.makeText(mMapActivity, msg, Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onProviderDisabled(String s) {
-                Log.d(TAG, "Dispositif désactivé : "+s);
+
+              Log.d(TAG, "Dispositif désactivé : "+s);
+                String msg = String.format("The provider"+ s +"is now disabled");
+                Toast.makeText(mMapActivity, msg, Toast.LENGTH_SHORT).show();
             }
         };
+    }
+
+    public Location getLocation () {
+        return location;
     }
 
 public double getLatitude() {
@@ -71,5 +87,17 @@ public double getLatitude() {
     }
     public boolean enableMyLocation() {
         return true;
+    }
+
+    public boolean sameLocation(Location _location) {
+        if (location.getLatitude() == _location.getLatitude()) {
+            if (location.getLongitude() == _location.getLongitude())
+                return true;
+        }
+        return false;
+    }
+
+    public LocationListener getLocationListener() {
+        return loc;
     }
 }
